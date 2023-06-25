9fdf222910fdebdc54c01014be3ee685778848bd
Troy <troydoropoulos@gmail.com>
2023-06-25 10:19:46 +0800

    Commented edits updated

--- app/core/management/Commands/wait_for_db.py
+++ app/core/management/Commands/wait_for_db.py
@@ -1,5 +1,5 @@
 """
-Django command to wait for the databse to be availbale.
+Django command to wait for the database to be available.
 """
 
 import time
@@ -9,7 +9,7 @@ from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
-class Comaamand(BaseCommand):
+class Comamand(BaseCommand):
     """Django command to wait for databse."""
 
     def handle(self, *args, **options):
