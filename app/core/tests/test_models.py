f385a46bc8d2c4501047f97fe08a2e5bc02fd086
Troy <troydoropoulos@gmail.com>
2023-06-25 15:58:06 +0800

    Updated test_models.py errors

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,16 +1,15 @@
 """
-Test for models.
+Tests for models.
 """
-
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
-    """ Test models."""
+    """Test models."""
 
     def test_create_user_with_email_successful(self):
-        """Test creating a user with an email is sucesssful."""
+        """Test creating a user with an email is sucessful."""
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
