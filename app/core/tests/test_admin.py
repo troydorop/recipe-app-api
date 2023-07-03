b73dfc03473e0ba90fdff88df3ff35bfd9813276
Troy <troydoropoulos@gmail.com>
2023-07-03 16:08:49 +0800

    Typos's test_admin.py

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -12,13 +12,13 @@ class AdminSiteTests(TestCase):
 
     def setUp(self):
         """Create user and client."""
-        self.cleint = Client()
+        self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
             email='admin@example.com',
             password='testpass123',
         )
         self.client.force_login(self.admin_user)
-        self.user = get_user_model().objects.creeate_user(
+        self.user = get_user_model().objects.create_user(
             email='user@example.com',
             password='testpass123',
             name='Test User'
