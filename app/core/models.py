11825081708ce8bb2a4fb3b7e13711ea602c6d47
Troy <troydoropoulos@gmail.com>
2023-07-03 10:46:55 +0800

    Cleaned up Models.py when running migrations

--- app/core/models.py
+++ app/core/models.py
@@ -19,8 +19,8 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
         """User in the system."""
-        email = models.EmailField(max_length=255, uniqrue=True)
-        name = models.Charfield(max_length=255)
+        email = models.EmailField(max_length=255, unique=True)
+        name = models.CharField(max_length=255)
         is_active = models.BooleanField(default=True)
         is_staff = models.BooleanField(default=False)
 
