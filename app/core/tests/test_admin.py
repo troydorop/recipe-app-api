35a7906297924ffedef5c3efbaa2c1db2050e815
Troy <troydoropoulos@gmail.com>
2023-07-04 11:28:53 +0800

    Typo's on test_admin

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,11 +1,11 @@
 """
-Tests for the Django admin modificaitons.
+Tests for the Django admin modifications.
 """
-
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import Client
+
 
 class AdminSiteTests(TestCase):
     """Tests for Django admin."""
@@ -24,7 +24,7 @@ class AdminSiteTests(TestCase):
             name='Test User'
         )
 
-    def test_users_list(self):
+    def test_users_lists(self):
         """Test that users are listed on page."""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
@@ -40,7 +40,7 @@ class AdminSiteTests(TestCase):
         self.assertEqual(res.status_code, 200)
 
     def test_create_user_page(self):
-        """Test the reate user page works."""
+        """Test the create user page works."""
         url = reverse('admin:core_user_add')
         res = self.client.get(url)
 
