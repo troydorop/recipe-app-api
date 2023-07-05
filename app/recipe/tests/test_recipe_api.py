cd8286cfe82b0fd04a6c1d405872683af3f858dd
Troy <troydoropoulos@gmail.com>
2023-07-05 14:27:53 +0800

    Write Tests API feild listings

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -15,3 +15,29 @@ from core.models import Recipe
 
 from recipe.serializers import RecipeSerializer
 
+RECIPES_URL = reverse('recipe:recipe-list')
+
+def create_recipe(user, **params):
+    """Create and return a sample recipe."""
+    defaults = {
+        'title': 'Sample recipe title',
+        'time_minutes': 22,
+        'price': Decimal('5.25'),
+        'description': 'Sample description',
+        'link': 'http://example.com/recipe.pdf',
+    }
+    defaults.update(params)
+
+    recipe = Recipe.objects.create(user=user, **defaults)
+    return recipe
+
+class PublicRecipeAPITests(TestCase):
+    """Test unauthenticated APIRequests."""
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_authrequired(self):
+        """Test auth is required to call API."""
+        res = self.client.get(RECIPES_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
